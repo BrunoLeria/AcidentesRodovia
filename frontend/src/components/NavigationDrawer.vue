8bcb2e05116eba0cef382cfb4ae5b49eafe9979b
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2023-03-29 08:43:00 -0300

    Campos de filtragem criados

--- frontend/src/components/NavigationDrawer.vue
+++ frontend/src/components/NavigationDrawer.vue
@@ -1,38 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
 const drawer = ref(true);
-const items = ref([
-  { title: 'Home', icon: 'mdi-home-city' },
-  { title: 'My Account', icon: 'mdi-account' },
-  { title: 'Users', icon: 'mdi-account-group-outline' },
-]);
+const items = ref(["California", "Colorado", "Florida", "Georgia", "Texas", "Wyoming"]);
 const rail = ref(true);
+const data = ref("");
+const hora = ref("");
+const local = ref("");
+const tipo_incidente = ref("");
+const km = ref("");
 </script>
 <template>
-  <v-card class="rounded-0">
-    <v-layout>
-      <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-magnify" title="Search">
-            <template v-slot:append>
-              <v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
-            </template>
-          </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home">
-          </v-list-item>
-          <v-list-item prepend-icon="mdi-account" title="My Account" value="account">
-          </v-list-item>
-          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users">
-          </v-list-item>
-        </v-list>
-        <v-text-field label="Label" variant="outlined"></v-text-field>
-
-      </v-navigation-drawer>
-      <v-main style="height: 250px"></v-main>
-    </v-layout>
-  </v-card>
+	<v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+		<v-list density="compact" nav>
+			<v-list-item prepend-icon="mdi-magnify" title="Search">
+				<template v-slot:append>
+					<v-btn variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+				</template>
+			</v-list-item>
+		</v-list>
+		<v-divider></v-divider>
+		<div v-show="!rail">
+			<v-label class="ma-5">Filtros</v-label>
+			<v-divider></v-divider>
+			<v-label class="mx-5 mt-5">Data:</v-label>
+			<v-text-field variant="outlined" v-show="!rail" class="mx-5 mb-5" type="date"></v-text-field>
+			<v-label class="mx-5">Hora:</v-label>
+			<v-text-field variant="outlined" v-show="!rail" class="mx-5 mb-5" type="time" suffix="BRL"></v-text-field>
+			<v-label class="mx-5">Local:</v-label>
+			<v-text-field variant="outlined" v-show="!rail" class="mx-5 mb-5"></v-text-field>
+			<v-label class="mx-5">Tipo do incidente:</v-label>
+			<v-combobox :items="items" variant="outlined" v-show="!rail" class="mx-5 mb-5"></v-combobox>
+			<v-label class="mx-5">KM:</v-label>
+			<v-text-field variant="outlined" v-show="!rail" class="mx-5 mb-5"></v-text-field>
+			<v-btn color="primary" class="mx-5 mb-5">Filtrar</v-btn>
+		</div>
+	</v-navigation-drawer>
 </template>
