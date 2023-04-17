f34b39278a0fa82d310aa10781064fb6ebac8e43
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2023-04-17 07:19:42 -0300

    Add user store and user interface with initial data and a verifyUser action

---
+++ frontend/src/interfaces/user.interface.ts
@@ -0,0 +1,5 @@
+export interface IUser {
+  readonly email: string;
+  readonly name: string;
+  readonly password: string;
+}
---
+++ frontend/src/store/users.ts
@@ -0,0 +1,46 @@
+import { IUser } from "@/interfaces/user.interface";
+import { defineStore } from "pinia";
+
+export const useUserStore = defineStore("user", {
+  state: () => [
+    {
+      id: 1,
+      email: "michael.lawson@reqres.in",
+      name: "Michael Lawson",
+      password: "!!Xz%Y5VuN",
+    },
+    {
+      id: 2,
+      email: "lindsay.ferguson@reqres.in",
+      name: "Lindsay Ferguson",
+      password: "@58Dv7ym6M",
+    },
+    {
+      id: 3,
+      email: "tobias.funke@reqres.in",
+      name: "Tobias Funke",
+      password: "4pwAM5&tWo",
+    },
+    {
+      id: 4,
+      email: "byron.fields@reqres.in",
+      name: "Byron Fields",
+      password: "7$J63g^V2n",
+    },
+    {
+      id: 5,
+      email: "george.edwards@reqres.in",
+      name: "George Edwards",
+      password: "i2S$DHxATx",
+    },
+  ],
+  getters: {},
+  actions: {
+    verifyUser(email: string, password: string): IUser | undefined {
+      const user = this.find(
+        (user: IUser) => user.email === email && user.password === password
+      );
+      return user;
+    },
+  },
+});
