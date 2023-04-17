51b005221f92c4e08a1e94f0757a51bcbe88a939
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2023-04-17 06:58:47 -0300

    Refactor Default.vue layout to use a more appropriate color scheme and container structure

--- frontend/src/layouts/default/Default.vue
+++ frontend/src/layouts/default/Default.vue
@@ -1,18 +1,18 @@
 <template>
-  <v-app>
-    <div class="d-flex flex-row mb-6 bg-surface-variant">
-      <default-bar v-model:drawer="drawer" />
-      <div class="d-flex flex-col mb-6 bg-surface-variant">
-        <default-view />
-      </div>
-    </div>
-  </v-app>
+	<v-app>
+		<div class="d-flex flex-row mb-6 bg-grey-darken-3 h-100">
+			<default-bar v-model:drawer="drawer" />
+			<v-container>
+				<default-view />
+			</v-container>
+		</div>
+	</v-app>
 </template>
 
 <script lang="ts" setup>
-import DefaultBar from './AppBar.vue'
-import DefaultView from './View.vue'
-import { ref } from 'vue';
+import DefaultBar from "./AppBar.vue";
+import DefaultView from "./View.vue";
+import { ref } from "vue";
 
 const drawer = ref(false);
 </script>
