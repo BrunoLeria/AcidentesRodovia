858f50a5424e499fded5f26c18d1267d141d3b2b
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2023-04-18 19:45:38 -0300

    Add props to Card component for title, color, name, and text, and update template to display props accordingly

--- frontend/src/components/Home/Panel/Card.vue
+++ frontend/src/components/Home/Panel/Card.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
+import { defineProps } from 'vue';
 
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    color: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+});
 </script>
 
 <template>
-    <v-card height="200" class="ma-2 pa-2"></v-card>
+    <v-card class="mx-auto" color="props.color" theme="dark" max-width="400" prepend-icon="mdi-twitter" title="props.title">
+        <template v-slot:prepend>
+            <v-icon size="x-large"></v-icon>
+        </template>
+
+        <v-card-subtitle class="text-h5 py-2">
+            {{ props.name }}
+        </v-card-subtitle>
+
+        <v-card-text class="text-h5 py-2">
+            {{ props.text }}
+        </v-card-text>
+    </v-card>
 </template>
