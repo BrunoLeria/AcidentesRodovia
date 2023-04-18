2cbf30d5c8c74c1e34c74d548031a2fac82c6c7c
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2023-04-18 14:27:46 -0300

    Add loading component to DefaultView async component definition

---
+++ frontend/src/components/Utils/Loading.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mx-auto px-6 py-8 bg-grey-darken-3">
+          <v-card-title class="d-flex justify-center text-h5">
+            Loading...
+          </v-card-title>
+          <v-card-text class="d-flex justify-center">
+            <v-progress-circular color="blue-lighten-3" indeterminate :size="65" :width="10" />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- frontend/src/layouts/default/Default.vue
+++ frontend/src/layouts/default/Default.vue
@@ -3,7 +3,7 @@
 		<div class="d-flex flex-row mb-6 bg-grey-darken-3 h-100">
 			<default-bar v-model:drawer="drawer" />
 			<v-container>
-				<default-view />
+				<DefaultView />
 			</v-container>
 		</div>
 	</v-app>
@@ -11,8 +11,19 @@
 
 <script lang="ts" setup>
 import DefaultBar from "./AppBar.vue";
-import DefaultView from "./View.vue";
-import { ref } from "vue";
+import { ref, defineAsyncComponent } from "vue";
+import LoadingComponent from "@/components/Utils/Loading.vue";
+
+
+const DefaultView = defineAsyncComponent({
+	loader: () => import("./View.vue"),
+	loadingComponent: LoadingComponent,
+	// Delay before showing the loading component. Default: 200ms.
+	delay: 200,
+	// The error component will be displayed if a timeout is
+	// provided and exceeded. Default: Infinity.
+	timeout: 3000
+});
 
 const drawer = ref(false);
 </script>
