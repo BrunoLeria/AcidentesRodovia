d886b5c52b9d1f24e191e17038a4c177d34d71eb
Bruno Leria <brunovolpatol@hotmail.com>
2023-04-19 17:27:41 -0300

    "Remove hard-coded user data from the user store initialization."

--- frontend/src/store/users.ts
+++ frontend/src/store/users.ts
@@ -4,38 +4,7 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore("user", {
   state: () => {
     return {
-      userList: [
-        {
-          id: 1,
-          email: "michael.lawson@reqres.in",
-          name: "Michael Lawson",
-          password: "!!Xz%Y5VuN",
-        },
-        {
-          id: 2,
-          email: "lindsay.ferguson@reqres.in",
-          name: "Lindsay Ferguson",
-          password: "@58Dv7ym6M",
-        },
-        {
-          id: 3,
-          email: "tobias.funke@reqres.in",
-          name: "Tobias Funke",
-          password: "4pwAM5&tWo",
-        },
-        {
-          id: 4,
-          email: "byron.fields@reqres.in",
-          name: "Byron Fields",
-          password: "7$J63g^V2n",
-        },
-        {
-          id: 5,
-          email: "george.edwards@reqres.in",
-          name: "George Edwards",
-          password: "i2S$DHxATx",
-        },
-      ],
+      userList: [],
       user: {} as IUser,
     };
   },
