3d636d7f76afed24ff3a291a8e00df006c47b874
Bruno Leria <brunovolpatol@hotmail.com>
2023-04-20 17:36:36 -0300

    Add utility functions for generating random numbers

---
+++ frontend/src/utils/NumbersTreatment.ts
@@ -0,0 +1,19 @@
+export function getRandom() {
+  return Math.random();
+}
+
+export function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+export function getRandomArbitrary(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
