3133ac39aa99846210fc052d95ccf73d76d7dfe4
Bruno Leria <brunovolpatol@hotmail.com>
2023-04-20 16:59:51 -0300

    Add occurrence store with getters, actions, and state in occurrences.ts. Include methods to get, add, update, and delete occurrences using API requests

---
+++ frontend/src/store/occurrences.ts
@@ -0,0 +1,169 @@
+import { defineStore } from "pinia";
+import { IOccurrence } from "@/interfaces/occurrence.interface";
+import { useUserStore } from "./users";
+import { getToday } from "@/utils/DateTimeTratement";
+
+const userStore = useUserStore();
+
+export const useOccurrenceStore = defineStore("user", {
+  state: () => {
+    return {
+      occurrences: [] as IOccurrence[],
+    };
+  },
+  getters: {},
+  actions: {
+    async getOccurrences(): Promise<boolean> {
+      const myHeaders = new Headers();
+
+      const requestOptions: RequestInit = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      const result = await fetch(
+        "http://localhost:5000/occurrences",
+        requestOptions
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((result) => {
+          if (result.hasOwnProperty("message")) {
+            alert(result.message);
+            return false;
+          } else {
+            this.$state.occurrences = result;
+            return true;
+          }
+        });
+
+      return result;
+    },
+    async addOccurrence(
+      local: string,
+      occurrence_tipe: string,
+      km: number
+    ): Promise<boolean> {
+      const myHeaders = new Headers();
+      myHeaders.append(
+        "x-token",
+        localStorage.getItem("token") || "no token found"
+      );
+      myHeaders.append("Content-Type", "application/json");
+
+      const raw = JSON.stringify({
+        registered_at: getToday(),
+        local: local,
+        occurrence_tipe: occurrence_tipe,
+        km: km,
+        user_id: userStore.user.id,
+      });
+
+      const requestOptions: RequestInit = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      const result = await fetch(
+        "http://localhost:5000/occurrences",
+        requestOptions
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((result) => {
+          if (result.hasOwnProperty("message")) {
+            alert(result.message);
+            return false;
+          } else {
+            this.$state.occurrences.push(result);
+            return true;
+          }
+        });
+
+      return result;
+    },
+    async updateUser(
+      id: number,
+      local: string,
+      occurrence_tipe: string,
+      km: number
+    ): Promise<boolean> {
+      const myHeaders = new Headers();
+      myHeaders.append(
+        "x-token",
+        localStorage.getItem("token") || "no token found"
+      );
+      myHeaders.append("Content-Type", "application/json");
+
+      const raw = JSON.stringify({
+        local: local,
+        occurrence_tipe: occurrence_tipe,
+        km: km,
+      });
+
+      const requestOptions: RequestInit = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      const result = await fetch(
+        `http://localhost:5000/occurrences/${id}`,
+        requestOptions
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((result) => {
+          alert(result.message);
+          return (
+            result.hasOwnProperty("message") &&
+            result.message === "Atualização da ocorrência realizada com sucesso"
+          );
+        });
+
+      if (result) this.getOccurrences();
+
+      return result;
+    },
+    async deleteUser(id: number): Promise<boolean> {
+      const myHeaders = new Headers();
+      myHeaders.append(
+        "x-token",
+        localStorage.getItem("token") || "no token found"
+      );
+
+      const requestOptions: RequestInit = {
+        method: "DELETE",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      const result = await fetch(
+        `http://localhost:5000/occurrences/${id}`,
+        requestOptions
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((result) => {
+          alert(result.message);
+          return (
+            result.hasOwnProperty("message") &&
+            result.message === "Ocorrência excluida com sucesso"
+          );
+        });
+
+      if (result) this.getOccurrences();
+
+      return result;
+    },
+  },
+  persist: true,
+});
