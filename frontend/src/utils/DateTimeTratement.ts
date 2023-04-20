aa3a0264440dc3ea0b634adba3ecd65ffa7b8c02
Bruno Leria <brunovolpatol@hotmail.com>
2023-04-20 16:59:27 -0300

    Add utility functions for time and date manipulation in frontend/src/utils/DateTimeTreatment.ts

---
+++ frontend/src/utils/DateTimeTratement.ts
@@ -0,0 +1,49 @@
+export function timeToSeconds(tempo: string) {
+  const [HOURS = 0, MINUTES = 0, SECONDS = 0] = tempo.split(":").map(Number);
+  const TIME_IN_SECONDS = HOURS * 3600 + MINUTES * 60 + SECONDS;
+  return TIME_IN_SECONDS;
+}
+export function secondsToTime(timeInSeconds: number) {
+  const NUMBERS_OF_HOURS = Math.floor(timeInSeconds / 3600);
+  const NUMBERS_OF_MINUTES = Math.floor(
+    (timeInSeconds - NUMBERS_OF_HOURS * 3600) / 60
+  );
+  const NUMBERS_OF_SECONDS =
+    timeInSeconds - NUMBERS_OF_HOURS * 3600 - NUMBERS_OF_MINUTES * 60;
+  const HOURS =
+    NUMBERS_OF_HOURS < 10 ? `0${NUMBERS_OF_HOURS}` : NUMBERS_OF_HOURS;
+  const MINUTES =
+    NUMBERS_OF_MINUTES < 10 ? `0${NUMBERS_OF_MINUTES}` : NUMBERS_OF_MINUTES;
+  const SECONDS =
+    NUMBERS_OF_SECONDS < 10 ? `0${NUMBERS_OF_SECONDS}` : NUMBERS_OF_SECONDS;
+  const time = `${HOURS}:${MINUTES}:${SECONDS}`;
+  return time;
+}
+
+export function getDateFull(date?: Date) {
+  const MONTH_NAMES = [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro",
+  ];
+  const DATE = new Date(date || Date.now());
+  const CURRENT_DATE = `${DATE.getDate()} de ${
+    MONTH_NAMES[DATE.getMonth()]
+  } de ${DATE.getFullYear()}`;
+  return CURRENT_DATE;
+}
+
+export function getToday(date?: Date) {
+  const DATE = new Date(date || Date.now());
+  const CURRENT_DATE = `${DATE.getDate()}/${DATE.getMonth()}/${DATE.getFullYear()}`;
+  return CURRENT_DATE;
+}
