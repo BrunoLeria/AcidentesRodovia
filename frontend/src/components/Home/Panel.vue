ba5053ffc84bc5fcc0fd27a5ce032ecde00336e6
Bruno Leria <brunovolpatol@hotmail.com>
2023-04-20 17:37:07 -0300

    Add functionality to fetch occurrences from API and display them as cards in Home Panel

--- frontend/src/components/Home/Panel.vue
+++ frontend/src/components/Home/Panel.vue
@@ -1,14 +1,71 @@
 <script setup lang="ts">
 import Card from "./Panel/Card.vue";
 import NavigationDrawer from "@/components/Home/Panel/NavigationDrawer.vue";
+import { useOccurrenceStore } from "@/store/occurrences";
+import { getRandomIntInclusive } from "@/utils/NumbersTreatment";
+import { onMounted } from "vue";
+
+const occurrencesStore = useOccurrenceStore();
+
+const getOccurrences = async () => {
+	const myHeaders = new Headers();
+
+	const requestOptions: RequestInit = {
+		method: "GET",
+		headers: myHeaders,
+		redirect: "follow",
+	};
+
+	await fetch(
+		"http://localhost:5000/occurrences",
+		requestOptions
+	)
+		.then((response) => {
+			return response.json();
+		})
+		.then((result) => {
+			if (result.hasOwnProperty("message")) {
+				alert(result.message);
+			} else {
+				occurrencesStore.occurrences = result;
+			}
+		});
+};
+
+onMounted(() => {
+	getOccurrences();
+});
+
+const colors = [
+	"red-lighten-1",
+	"pink-lighten-1",
+	"purple-lighten-1",
+	"deep-purple-lighten-1",
+	"indigo-lighten-1",
+	"blue-lighten-1",
+	"light-blue-lighten-1",
+	"cyan-lighten-1",
+	"teal-lighten-1",
+	"green-lighten-1",
+	"light-green-lighten-1",
+	"lime-lighten-1",
+	"yellow-lighten-1",
+	"amber-lighten-1",
+	"orange-lighten-1",
+	"deep-orange-lighten-1",
+	"brown-lighten-1",
+	"grey-lighten-1",
+	"blue-grey-lighten-1",
+]
+
 </script>
 <template>
 	<div class="d-flex flex-row mb-6 bg-surface-variant">
 		<NavigationDrawer />
 		<v-container>
 			<v-row>
-				<v-col v-for="n in 24" :key="n" cols="6" sm="4">
-					<!-- <Card /> -->
+				<v-col v-for="occurrence in occurrencesStore.occurrences" :key="occurrence.id" cols="6" sm="4">
+					<Card :occurrence="occurrence" :color="colors[getRandomIntInclusive(0, colors.length - 1)]" />
 				</v-col>
 			</v-row>
 		</v-container>
--- frontend/src/components/Home/Panel/Card.vue
+++ frontend/src/components/Home/Panel/Card.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
+import { IOccurrence } from "@/interfaces/occurrence.interface";
 
 const props = defineProps({
-    title: {
-        type: String,
+    occurrence: {
+        type: Object as () => IOccurrence,
         required: true,
     },
     color: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    text: {
         type: String,
         required: true,
     },
@@ -22,17 +15,18 @@ const props = defineProps({
 </script>
 
 <template>
-    <v-card class="mx-auto" color="props.color" theme="dark" max-width="400" prepend-icon="mdi-twitter" title="props.title">
-        <template v-slot:prepend>
-            <v-icon size="x-large"></v-icon>
-        </template>
+    <v-card class="mx-auto" :color="props.color" theme="dark" max-width="400" prepend-icon="m🔽 "
+        :title="props.occurrence.local">
 
-        <v-card-subtitle class="text-h5 py-2">
-            {{ props.name }}
+        <v-card-subtitle class="text-h6 py-2">
+            Km: {{ props.occurrence.km }}
         </v-card-subtitle>
+        <v-divider />
 
-        <v-card-text class="text-h5 py-2">
-            {{ props.text }}
+        <v-card-text class="text-h8 py-2">
+            Registrado em: {{ props.occurrence.registered_at }}
+            Pelo usuário: {{ props.occurrence.user_id }}
+            Tipo de ocorrência: {{ props.occurrence.occurrence_tipe }}
         </v-card-text>
     </v-card>
 </template>
