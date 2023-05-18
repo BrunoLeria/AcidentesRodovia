f30a7f758ae55d1d3828294620e2950d298e4e9d
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-18 17:12:39 -0300

    Solved a problem related to localhost. It was trying to connect using ipv6.

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
-const uri = "mongodb://localhost:27017/";
+const uri = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(uri);
 const database = client.db("acidentesDB");
 const users = database.collection("users");
