cd58fb3ffe8bd84e4efac9dd6b74337cb977edcb
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-18 03:05:55 -0300

    Add new getOccurrences function to occurrence store to fetch occurrences based on date, local, occurrence type, and km

--- frontend/src/store/occurrences.ts
+++ frontend/src/store/occurrences.ts
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 import { IOccurrence } from "@/interfaces/occurrence.interface";
 import { useUserStore } from "./users";
 import { getToday } from "@/utils/DateTimeTreatment";
+import { getDateForMongo } from "../utils/DateTimeTreatment";
 
 const userStore = useUserStore();
 
@@ -47,6 +48,7 @@ export const useOccurrenceStore = defineStore("occurrence", {
         .then((response) => {
           if (response.status === 401) {
             alert("Sessão expirada, faça login novamente");
+            localStorage.setItem("token", "");
             window.location.href = "/login";
             return false;
           }
@@ -97,6 +99,7 @@ export const useOccurrenceStore = defineStore("occurrence", {
         .then((response) => {
           if (response.status === 401) {
             alert("Sessão expirada, faça login novamente");
+            localStorage.setItem("token", "");
             window.location.href = "/login";
             return false;
           }
@@ -113,6 +116,44 @@ export const useOccurrenceStore = defineStore("occurrence", {
       if (result) {
         location.reload();
       }
+      return result;
+    },
+    async getOccurrences(
+      date: string,
+      local: string,
+      occurrence_tipe: string,
+      km: string
+    ): Promise<boolean> {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const requestOptions: RequestInit = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      date = !date ? date : getDateForMongo(date);
+
+      const result = await fetch(
+        `http://localhost:5000/occurrences/?
+registered_at=${date}&local=${local}&occurrence_tipe=${occurrence_tipe}&km=${km}`,
+        requestOptions
+      )
+        .then((response) => {
+          if (response.status === 404) {
+            alert("Nenhuma ocorrência encontrada");
+            return false;
+          }
+          return response.json();
+        })
+        .then((result) => {
+          if (!result) return false;
+          if (!result.hasOwnProperty("message")) {
+            this.$state.occurrences = result;
+          }
+          return !result.hasOwnProperty("message");
+        });
       return result;
     },
     async deleteOccurrence(id: number): Promise<boolean> {
@@ -135,6 +176,7 @@ export const useOccurrenceStore = defineStore("occurrence", {
         .then((response) => {
           if (response.status === 401) {
             alert("Sessão expirada, faça login novamente");
+            localStorage.setItem("token", "");
             window.location.href = "/login";
             return false;
           }
