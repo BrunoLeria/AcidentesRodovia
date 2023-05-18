c6a5d1dd84b33618060e70e48bd1e32b3b6508d1
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-18 03:05:10 -0300

    Add occurrenceType array to TypeOfOccurrences utils

---
+++ frontend/src/utils/TypeOfOccurrences.ts
@@ -0,0 +1,18 @@
+import { ref } from "vue";
+
+export const occurrenceType = ref([
+  "Colisão traseira",
+  "Saída de pista",
+  "Abalroamento lateral mesmo sentido",
+  "Choque com objeto fixo",
+  "Abalroamento transversal",
+  "Atropelamento",
+  "Abalroamento lateral sentido oposto",
+  "Atropelamento de animal",
+  "Capotagem",
+  "Tombamento",
+  "Colisão frontal",
+  "Atropelamento e fuga",
+  "Choque com veiculo estacionado",
+  "Outros tipos",
+]);
