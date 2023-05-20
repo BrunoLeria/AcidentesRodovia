b2bc10b294dea593e99a23d6ba9b3605001db66c
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-20 14:36:12 -0300

    Add user stub for testing user schema in the Nest backend

---
+++ nest-backend/src/users/test/stubs/user.stub.ts
@@ -0,0 +1,12 @@
+import { User } from '../../schemas/user.schema';
+import { Types } from 'mongoose';
+
+export const userStub = (): User => {
+  return {
+    _id: new Types.ObjectId('5f9d4a3d9d6c2c1f1c9d4408'),
+    userId: '9',
+    email: 'michael.lawson@reqres.in',
+    name: 'Michael Lawson',
+    password: '$2b$10$isaOSpX9mUZ7PKi0SThkSujtUqrk4iaNh9zfiyZz2ocEOTj2QlGKW',
+  };
+};
