be579eea1e0281a53026968923203d9e97a1fa6f
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-20 14:38:09 -0300

    Add MongooseModule and UsersRepository to UsersModule for better database integration and extendability

--- nest-backend/src/users/users.module.ts
+++ nest-backend/src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService],
 })
 export class UsersModule {}
