8b85c20f3eb012f027286244ad04b5474a529bd3
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-20 14:44:09 -0300

    Add global configuration to app.module.ts with validation schema and environment file path

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        MONGODB_DB: Joi.string().required(),
+        PORT: Joi.number().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+      }),
+      envFilePath: './.env',
+    }),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
