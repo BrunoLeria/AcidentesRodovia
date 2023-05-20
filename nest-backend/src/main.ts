f785dd0ed4d658b1da30fe69be3cbf55387a6832
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-20 14:44:17 -0300

    Add global validation pipe and use config service to listen on the configured port in Nest backend's main.ts file. Also, add hot module replacement support for development

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
