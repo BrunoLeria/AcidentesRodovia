004957d4ab40628690322010cee13ba4a8c3a47a
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-21 21:53:10 -0300

    Refactor getCurrentUserByContext to remove unnecessary line breaks and improve readability

--- nest-backend/src/auth/current-user.decorator.ts
+++ nest-backend/src/auth/current-user.decorator.ts
@@ -1,9 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '../users/schemas/user.schema';
 
-export const getCurrentUserByContext = (
-  context: ExecutionContext,
-): User => {
+export const getCurrentUserByContext = (context: ExecutionContext): User => {
   if (context.getType() === 'http') {
     return context.switchToHttp().getRequest().user;
   }
