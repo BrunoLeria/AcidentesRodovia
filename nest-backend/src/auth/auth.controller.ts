97256e17d4ab36bf8f34951df3e8d3b1251819fe
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-21 22:06:04 -0300

    Executed lint and format

--- nest-backend/src/auth/auth.controller.ts
+++ nest-backend/src/auth/auth.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Controller,
-  HttpCode,
-  Post,
-  UseGuards,
-  Res,
-  Get,
-} from '@nestjs/common';
+import { Controller, HttpCode, Post, UseGuards, Res } from '@nestjs/common';
 import { CurrentUser } from './current-user.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Response } from 'express';
@@ -13,13 +6,13 @@ import { User } from '../users/schemas/user.schema';
 import { AuthService } from './auth.service';
 import JwtAuthGuard from './guards/jwt-auth.guard';
 
-@Controller('login')
+@Controller('')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(200)
   @UseGuards(LocalAuthGuard)
-  @Post()
+  @Post('login')
   async login(
     @CurrentUser() user: User,
     @Res({ passthrough: true }) response: Response,
@@ -29,7 +22,7 @@ export class AuthController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get()
+  @Post('')
   async logout(@Res({ passthrough: true }) response: Response) {
     await this.authService.logout(response);
     response.send();
