5b672165090d809ee9a519d38bf73f88c9673f46
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-21 21:52:37 -0300

    Add JWTAuthGuard to protect the create, update, delete and get by user operations in the occurrences controller

--- nest-backend/src/occurrences/occurrences.controller.ts
+++ nest-backend/src/occurrences/occurrences.controller.ts
@@ -6,17 +6,20 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { OccurrencesService } from './occurrences.service';
 import { CreateOccurrenceDto } from './dto/create-occurrences.dto';
 import { Occurrence } from './schemas/occurrence.schema';
 import { UpdateOccurrencesDto } from './dto/update-occurrences.dto';
+import JwtAuthGuard from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('occurrences')
 export class OccurrencesController {
   constructor(private readonly occurrencesService: OccurrencesService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async createOccurrence(
     @Body() createOccurrenceDto: CreateOccurrenceDto,
   ): Promise<Occurrence> {
@@ -28,6 +31,7 @@ export class OccurrencesController {
     return await this.occurrencesService.getOccurrences();
   }
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   async updateOccurrence(
     @Param('id') id: string,
     @Body() updateOccurrenceDto: UpdateOccurrencesDto,
@@ -39,12 +43,16 @@ export class OccurrencesController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async deleteOccurrence(@Param('id') id: string): Promise<Occurrence> {
     return await this.occurrencesService.deleteOccurrence(id);
   }
 
   @Get('users/:id')
+  @UseGuards(JwtAuthGuard)
   async getOccurrencesByUser(@Param('id') id: string): Promise<Occurrence> {
-    return await this.occurrencesService.getOccurrence({ userId: id });
+    return await this.occurrencesService.getOccurrence({
+      user_id: parseInt(id),
+    });
   }
 }
