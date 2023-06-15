971630d0ae1ba2fa32c8def19c451472e45d244e
Bruno Leria <brunovolpatol@hotmail.com>
2023-06-15 02:45:57 -0300

    Add validation rules to CreateOccurrenceDto

--- nest-backend/src/occurrences/dto/create-occurrences.dto.ts
+++ nest-backend/src/occurrences/dto/create-occurrences.dto.ts
@@ -1,23 +1,38 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Matches,
+  Max,
+  MaxLength,
+  Min,
+} from 'class-validator';
 
 export class CreateOccurrenceDto {
   @IsString()
   @IsNotEmpty()
+  @Matches(/^\d{4}[-]\d{2}[-]\d{2}[T]\d{2}[:]\d{2}[:]\d{2}[.]\d{3}[Z]$/)
   registered_at;
 
   @IsString()
   @IsNotEmpty()
+  @MaxLength(125)
   local;
 
   @IsNumber()
   @IsNotEmpty()
+  @Min(1)
+  @Max(10)
   occurrence_type;
 
   @IsNumber()
   @IsNotEmpty()
+  @Min(1)
+  @Max(9999)
   km;
 
   @IsNumber()
   @IsNotEmpty()
+  @Min(1)
   user_id;
 }
