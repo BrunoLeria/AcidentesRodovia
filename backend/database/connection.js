79b26701ebb880824e287322e685d88dba69c357
Bruno Leria <brunovolpatol@hotmail.com>
2023-06-15 15:37:01 -0300

    Deleting backend using Node.

--- backend/database/connection.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
-const uri = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(uri);
-const database = client.db("acidentesDB");
-const users = database.collection("users");
-const occurrences = database.collection("occurrences");
-
-module.exports = { users, occurrences };
